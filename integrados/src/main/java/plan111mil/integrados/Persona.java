96ba148505ba7e33665df729aef58b547b290286
parmani <pazarmani@gmail.com>
2019-01-04 12:52:15 -0300

    Agregando atributo id

--- integrados/src/main/java/plan111mil/integrados/Persona.java
+++ integrados/src/main/java/plan111mil/integrados/Persona.java
@@ -1,8 +1,11 @@
 package plan111mil.integrados;
 
 public class Persona {    
-	
-private int edad;
+
+	private String nombre;
+	private int edad;
+	private int id;
+
 
     public Persona() {
     }
@@ -11,5 +14,9 @@ private int edad;
         this.edad = edad;
     }
     
+    public Persona (int edad, String nombre) {
+    	this.edad=edad;
+    	this.nombre=nombre;
+    }
     
 }
