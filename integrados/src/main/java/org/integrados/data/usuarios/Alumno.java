d2ad301c3a735d2f7d27368b561c92b1a1caab6b
parmani <pazarmani@gmail.com>
2019-01-22 16:15:23 -0300

    Se cargan los Atributos,constructores,getter and setter

--- integrados/src/main/java/org/integrados/data/usuarios/Alumno.java
+++ integrados/src/main/java/org/integrados/data/usuarios/Alumno.java
@@ -5,10 +5,97 @@
  */
 package org.integrados.data.usuarios;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.integrados.data.actividad.ActividadAlumno;
+import org.integrados.data.enums.Nivel;
+
 /**
  *
  * @author Yani
  */
 public class Alumno extends Persona {
-    
+
+    private List<ActividadAlumno> actividades = new ArrayList<>();
+    private Date fechaNacimiento;
+    private int edadMadurativa;
+    private int grado;
+    private String division;
+    private Nivel nivel;
+    private String observaciones;
+
+    public Alumno(int dni, String nombre, String apellido, int grado, String division) {
+        super(dni, nombre, apellido);
+        this.grado = grado;
+        this.division = division;
+    }
+
+    public Alumno(int dni, String nombre, String apellido, String usuario, String clave, int grado, String division) {
+        super(dni, nombre, apellido, usuario, clave);
+        this.grado = grado;
+        this.division = division;
+    }
+
+    public List<ActividadAlumno> getActividades() {
+        return actividades;
+    }
+
+    public void setActividades(List<ActividadAlumno> actividades) {
+        this.actividades = actividades;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdadMadurativa() {
+        return edadMadurativa;
+    }
+
+    public void setEdadMadurativa(int edadMadurativa) {
+        this.edadMadurativa = edadMadurativa;
+    }
+
+    public int getGrado() {
+        return grado;
+    }
+
+    public void setGrado(int grado) {
+        this.grado = grado;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public Nivel getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Nivel nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "actividades=" + actividades + ", fechaNacimiento=" + fechaNacimiento + ", edadMadurativa=" + edadMadurativa + ", grado=" + grado + ", division=" + division + ", nivel=" + nivel + ", observaciones=" + observaciones + '}';
+    }
+
 }
--- integrados/src/main/java/org/integrados/data/usuarios/Domicilio.java
+++ integrados/src/main/java/org/integrados/data/usuarios/Domicilio.java
@@ -10,5 +10,70 @@ package org.integrados.data.usuarios;
  * @author Yani
  */
 public class Domicilio {
-    
+
+    private static int idActual = 0;
+    private int idDomicilio;
+    private String calle;
+    private int numero;
+    private String piso;
+    private String departamento;
+
+    public Domicilio() {
+        idActual++;
+        this.idDomicilio = idActual;
+    }
+
+    public Domicilio(String calle, int numero) {
+        idActual++;
+        this.idDomicilio = idActual;
+        this.calle = calle;
+        this.numero = numero;
+    }
+
+    public Domicilio(String calle, int numero, String piso, String departamento) {
+        idActual++;
+        this.idDomicilio = idActual;
+        this.calle = calle;
+        this.numero = numero;
+        this.piso = piso;
+        this.departamento = departamento;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Domicilio{" + "idDomicilio=" + idDomicilio + ", calle=" + calle + ", numero=" + numero + ", piso=" + piso + ", departamento=" + departamento + '}';
+    }
+
 }
