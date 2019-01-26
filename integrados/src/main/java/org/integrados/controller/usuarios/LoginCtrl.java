fe1ec6c8f1b0f97280099bee3ebd397b9c273628
Yani <yanina.gallardo30@gmail.com>
2019-01-25 21:23:50 -0300

    actualizacion (casos de uso, programa integrados de java)

--- integrados/src/main/java/org/integrados/controller/usuarios/LoginCtrl.java
+++ integrados/src/main/java/org/integrados/controller/usuarios/LoginCtrl.java
@@ -10,5 +10,48 @@ package org.integrados.controller.usuarios;
  * @author Mariela
  */
 public class LoginCtrl {
+    /*
+    +login
+    +validar usuario
+    +finalizar
+    +cancel (cierra pantalla) en login cierra sesion integrados.finalizar
     
+    login ctrl {
+    logionDlg logindlg =null;
+    aplicacion app;
+    LoginCtrl(Aplicacion app){
+    this.app = app;
+    }
+    
+    +login(){
+    loginDig dlg = new LoginDig(this);
+    dis.mostrar();   //metodo en view loginDlg.
+    }
+    
+    +validar(String usuario, String clave) throws validacionException{
+    
+    Session session = hibernateUtiles.getSession():
+    session.begintTransaction(); // hecho en clase personaABMCtrl
+    
+    Docente docente = session.get("From docte where usuario = " + usuario + " and clave= " + clave);  //REVISAR EL METODO EN LAS FILMINAS.
+     //SE VERIFICA EN QUE TABLA SE ENCUENTRA EL USUARIO INGRESADO.
+    if(docente !=null){
+    persona = docente;return;
+    }
+    
+    alumno alumno = ----------
+    if (alumno = null)  //DEVUELVE UNA EXCEPTION.  NO SE ENCUENTRA EL USUARIO EN LA BASE
+    
+    +finalizar(){
+        this.app.setPersonalogueada(persona);
+        this.logindlg.cerrar();
+    this.app.finalizar();
+    }
+    
+    +cancelar(){
+        this.logindlg.cerrar()
+        thiis.app.finalizar();
+    }
+    
+    */
 }
--- integrados/src/main/java/org/integrados/view/LoginDlg.java
+++ integrados/src/main/java/org/integrados/view/LoginDlg.java
@@ -4,5 +4,43 @@
 package org.integrados.view;
 
 public class LoginDlg {
+    /*
+    loginDlg 
+    loginctl controlador = null;
     
+    textUsuario = textField;
+    textClave = passwordField;
+    
+    +windows JFrame 
+    logindlg(loginctl controlador)
+    this.---- = ---;
+    contruirUI();
+    
+    
+    +mostrar(){
+    window.setVisible(true);
+    }
+    
+    boton ingresar:
+    
+    ingresar = new JbOTTON()
+    ingresar.addActionListener (new actionListener()[Interfaz]){  //agrega cpmo clase anonima
+    
+    @override
+    actionPerformer(-----){
+    validar del controlador.
+    try{
+    loginctl.validar(txtUsuario.getText(), textClave.getText());   //getText metodo que devuelve lo que ingresa el usuario.
+    }catch(exception e){
+        dialogo.mostrarError(e);
+    return;
+        }
+    }
+    
+    loginctrl.finalizar();
+    
+    actionPerfomer(){
+    loginctrl.cancelarLogin();
+    }
+    */
 }
