2fffde5a423e6a77b913b12515e1364aab77aad1
Yani <yanina.gallardo30@gmail.com>
2019-01-26 15:12:29 -0300

    se acomodo la clase ordenamiento

--- integrados/src/main/java/org/integrados/data/plantillas/Ordenamiento.java
+++ integrados/src/main/java/org/integrados/data/plantillas/Ordenamiento.java
@@ -20,7 +20,7 @@ public class Ordenamiento extends Plantilla {
         }
         return listaClonada;
     }
-
+    
     @Override
     public List<Bloque> desordenar() {
        int cambios = 0;
