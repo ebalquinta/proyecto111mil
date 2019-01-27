fe6dffcf94b3a30e09549c805d19f00c9da5090d
Jacco <Jaccogriffioen@gmail.com>
2019-01-27 00:43:56 -0300

    estoy armando y modificando los pojos de actividades la clase actividad alumno no la termine del todo

--- integrados/src/main/java/org/integrados/data/actividad/ActividadAlumno.java
+++ integrados/src/main/java/org/integrados/data/actividad/ActividadAlumno.java
@@ -5,121 +5,50 @@
  */
 package org.integrados.data.actividad;
 
-import java.util.Date;
+import java.util.List;
+import org.integrados.data.bloques.Bloque;
 import org.integrados.data.usuarios.Alumno;
-
+import java.util.Date;
 /**
  *
- * @author Yani
+ * @author Yani, Jacco
  */
 public class ActividadAlumno {
-
+    
     private Integer id;
     private Actividad actividad;
     private Alumno alumno;
+    private List<Bloque> respuestaAlumno;
     private int tiempo;
     private Date fechaInicio;
     private Date fechaFinal;
     private boolean finalizoCorrectamente;
     private int intentos;
-    private Recompensa recompensas;
+    private int estrella;
+    private String corazon;
     private String observaciones;
+            
+    public ActividadAlumno(){ }
 
-    public ActividadAlumno(Actividad actividad, Alumno alumno, int tiempo, Date fechaInicio, Date fechaFinal, boolean finalizoCorrectamente, int intentos) {
+    public ActividadAlumno(Actividad actividad, Alumno alumno, List<Bloque> respuestaAlumno, int tiempo, Date fechaInicio, Date fechaFinal, boolean finalizoCorrectamente, int intentos, int estrella) {
         this.actividad = actividad;
         this.alumno = alumno;
+        this.respuestaAlumno = respuestaAlumno;
         this.tiempo = tiempo;
         this.fechaInicio = fechaInicio;
         this.fechaFinal = fechaFinal;
         this.finalizoCorrectamente = finalizoCorrectamente;
         this.intentos = intentos;
+        this.estrella = estrella;
     }
-
-    public ActividadAlumno(Actividad actividad, Alumno alumno, int tiempo, Date fechaInicio, Date fechaFinal, boolean finalizoCorrectamente, int intentos, Recompensa recompensas) {
-        this(actividad, alumno, tiempo, fechaInicio, fechaFinal, finalizoCorrectamente, intentos);
-        this.recompensas = recompensas;
+    
+    /**
+     * 
+     */
+    public double calcularMinutosJugados(){
+       
     }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Actividad getActividad() {
-        return actividad;
-    }
-
-    public void setActividad(Actividad actividad) {
-        this.actividad = actividad;
-    }
-
-    public Alumno getAlumno() {
-        return alumno;
-    }
-
-    public void setAlumno(Alumno alumno) {
-        this.alumno = alumno;
-    }
-
-    public int getTiempo() {
-        return tiempo;
-    }
-
-    public void setTiempo(int tiempo) {
-        this.tiempo = tiempo;
-    }
-
-    public Date getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(Date fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public Date getFechaFinal() {
-        return fechaFinal;
-    }
-
-    public void setFechaFinal(Date fechaFinal) {
-        this.fechaFinal = fechaFinal;
-    }
-
-    public boolean isFinalizoCorrectamente() {
-        return finalizoCorrectamente;
-    }
-
-    public void setFinalizoCorrectamente(boolean finalizoCorrectamente) {
-        this.finalizoCorrectamente = finalizoCorrectamente;
-    }
-
-    public int getIntentos() {
-        return intentos;
-    }
-
-    public void setIntentos(int intentos) {
-        this.intentos = intentos;
-    }
-
-    public Recompensa getRecompensas() {
-        return recompensas;
-    }
-
-    public void setRecompensas(Recompensa recompensas) {
-        this.recompensas = recompensas;
-    }
-
-    public String getObservaciones() {
-        return observaciones;
-    }
-
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
-
+    
     @Override
     public String toString() {
         return "ActividadAlumno{" + "id=" + id + ", actividad=" + actividad + ", alumno=" + alumno + ", tiempo=" + tiempo + ", fechaInicio=" + fechaInicio + ", fechaFinal=" + fechaFinal + ", finalizoCorrectamente=" + finalizoCorrectamente + ", intentos=" + intentos + ", recompensas=" + recompensas + ", observaciones=" + observaciones + '}';
