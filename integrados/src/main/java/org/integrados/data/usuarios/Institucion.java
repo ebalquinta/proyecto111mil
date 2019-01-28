5ee07aedb9945ca4bfcb41e7de6d4f7c05bc51f4
leandro16197 <leandroovejero16197@hotmail.com>
2019-01-28 20:56:52 -0300

    Actualisacion Clase Institucion

--- integrados/src/main/java/org/integrados/data/usuarios/Institucion.java
+++ integrados/src/main/java/org/integrados/data/usuarios/Institucion.java
@@ -18,7 +18,7 @@ public class Institucion {
     private String nombreInstitucion;
     private List<Docente> docentes;
     private List<Alumno> alumnos;
-    private Persona administrador;
+    private Docente administrador;
 
     public Institucion() {
         this.alumnos = new ArrayList<>();
@@ -26,7 +26,7 @@ public class Institucion {
         this.id = null;
     }
 
-    public Institucion(String nombreInstitucion, Persona administrador) {
+    public Institucion(String nombreInstitucion, Docente administrador) {
         this.alumnos = new ArrayList<>();
         this.docentes = new ArrayList<>();
         this.id = null;
@@ -62,11 +62,11 @@ public class Institucion {
         this.alumnos = alumnos;
     }
 
-    public Persona getAdministrador() {
+    public Docente getAdministrador() {
         return administrador;
     }
 
-    public void setAdministrador(Persona administrador) {
+    public void setAdministrador(Docente administrador) {
         this.administrador = administrador;
     }
 
