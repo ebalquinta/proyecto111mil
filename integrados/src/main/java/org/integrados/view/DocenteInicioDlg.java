a7978a0fdd2accd5e4cf12d543b68dd45115fbf8
Yani <yanina.gallardo30@gmail.com>
2019-03-04 16:14:28 -0300

    llama a la ventana lista de alumnos del docente ingresante

--- integrados/src/main/java/org/integrados/view/DocenteInicioDlg.java
+++ integrados/src/main/java/org/integrados/view/DocenteInicioDlg.java
@@ -3,6 +3,7 @@
  */
 package org.integrados.view;
 
+import org.integrados.view.estadisticas.ServiciosEstadisticaCtrl;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,15 +11,15 @@ import javax.swing.*;
 import org.integrados.data.util.Util;
 import org.integrados.controller.actividades.DocenteBrowseActividadesCtrl;
 import org.integrados.controller.usuarios.DocenteInicioCtrl;
-import org.integrados.services.ServiciosEstadisticaCtrl;
-        
+
 public class DocenteInicioDlg extends JFrame {
+
     private JLabel lblFondo;
     private JButton btnActividades;
     private JButton btnAlumnos;
     private JButton btnCerrarSesion;
     private DocenteInicioCtrl docenteInicioCtrl;
-    
+
     public DocenteInicioDlg(DocenteInicioCtrl docenteInicioCtrl) {
         this.docenteInicioCtrl = docenteInicioCtrl;
         initComponents();
@@ -29,12 +30,12 @@ public class DocenteInicioDlg extends JFrame {
     }
 
     void initComponents() {
-        
+
         btnActividades = Util.crearBoton("Actividades", 18);
         btnAlumnos = Util.crearBoton("Alumnos", 18);
         btnCerrarSesion = Util.crearBoton("Cerrar Sesión", 14);
         lblFondo = new JLabel();
-        
+
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
@@ -45,43 +46,43 @@ public class DocenteInicioDlg extends JFrame {
         getContentPane().setLayout(null);
 
         // Propiedades del botón Actividades
-        btnActividades.setBounds(230, 320, 170, 60);       
+        btnActividades.setBounds(230, 320, 170, 60);
         getContentPane().add(btnActividades);
         DocenteInicioDlg aux = this;
         btnActividades.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae) {                
+            public void actionPerformed(ActionEvent ae) {
                 // Aquí debemos enlazar con el browser
                 DocenteBrowseActividadesCtrl ctrl = new DocenteBrowseActividadesCtrl(aux);
                 ctrl.mostrarBrw();
                 ocultar();
             }
         });
-        
+
         // Propiedades del botón Alumnos
         btnAlumnos.setBounds(400, 120, 170, 60);
         getContentPane().add(btnAlumnos);
         btnAlumnos.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-               ServiciosEstadisticaCtrl ctrl = new ServiciosEstadisticaCtrl(aux);
+                DocenteBrowseAlumnoCtrl ctrl = new DocenteBrowseAlumnoCtrl(aux);  // cambiar a ventana DocenteAlumnoBrowser
                 ctrl.mostrarBrw();
                 ocultar();
             }
         });
-        
+
         // Propiedades de botón Volver
         btnCerrarSesion.setBounds(295, 510, 170, 30);
         getContentPane().add(btnCerrarSesion);
         btnCerrarSesion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                docenteInicioCtrl.cerrarSesion();    
+                docenteInicioCtrl.cerrarSesion();
             }
         });
-        
+
         // Propiedades del fondo de pantalla
-        ImageIcon icon = createImageIcon("images/DocenteInicioDlgBkg.jpg","descripción");
+        ImageIcon icon = createImageIcon("images/DocenteInicioDlgBkg.jpg", "descripción");
         lblFondo.setIcon(icon);
         getContentPane().add(lblFondo);
         lblFondo.setBounds(0, 0, 800, 600);
@@ -101,13 +102,13 @@ public class DocenteInicioDlg extends JFrame {
             return null;
         }
     }
-    
+
     public void mostrar() {
         this.setVisible(true);
     }
-    
+
     public void ocultar() {
         this.setVisible(false);
-    }   
-    
+    }
+
 }
