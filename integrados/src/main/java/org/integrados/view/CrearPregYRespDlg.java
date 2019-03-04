a48934b46f4ffe04752e9e260df19b3c559b7d72
Mariela Gonzalez <mariela21180@gmail.com>
2019-03-04 16:43:49 -0300

    Swing - CrearPregYResp - Limpiando imports

--- integrados/src/main/java/org/integrados/view/CrearPregYRespDlg.java
+++ integrados/src/main/java/org/integrados/view/CrearPregYRespDlg.java
@@ -10,7 +10,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.table.*;
-import javax.swing.WindowConstants;
+import org.netbeans.lib.awtextra.*;
 import org.integrados.controller.actividades.CrearPregYRespCtrl;
 import org.integrados.data.usuarios.Docente;
 
@@ -129,27 +129,27 @@ public class CrearPregYRespDlg  extends JFrame {
         btnCancelar = new JButton();
 
         setBackground(new Color(0, 102, 102));
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().setLayout(new AbsoluteLayout());
 
         txtMaxIntentos.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
-        getContentPane().add(txtMaxIntentos, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 60, 30, 30));
-        getContentPane().add(linePregunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 670, 10));
+        getContentPane().add(txtMaxIntentos, new AbsoluteConstraints(650, 60, 30, 30));
+        getContentPane().add(linePregunta, new AbsoluteConstraints(60, 160, 670, 10));
 
         lblRespuestas.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         lblRespuestas.setText("Respuestas(Ingrese al menos 2 opciones)");
-        getContentPane().add(lblRespuestas, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, 340, 30));
+        getContentPane().add(lblRespuestas, new AbsoluteConstraints(70, 330, 340, 30));
 
         lblIngreseImagen.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         lblIngreseImagen.setText("Ingrese imagen");
-        getContentPane().add(lblIngreseImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, 110, 30));
+        getContentPane().add(lblIngreseImagen, new AbsoluteConstraints(80, 240, 110, 30));
 
         lblIngreseSonido.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         lblIngreseSonido.setText("Ingrese sonido");
-        getContentPane().add(lblIngreseSonido, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 280, 110, 30));
+        getContentPane().add(lblIngreseSonido, new AbsoluteConstraints(80, 280, 110, 30));
 
         lblIngreseTexto.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         lblIngreseTexto.setText("Ingrese texto");
-        getContentPane().add(lblIngreseTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 200, 110, 30));
+        getContentPane().add(lblIngreseTexto, new AbsoluteConstraints(80, 200, 110, 30));
 
         txtIngreseImagen.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         txtIngreseImagen.addActionListener(new ActionListener() {
@@ -157,7 +157,7 @@ public class CrearPregYRespDlg  extends JFrame {
                 txtIngreseImagenActionPerformed(evt);
             }
         });
-        getContentPane().add(txtIngreseImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 240, 230, 30));
+        getContentPane().add(txtIngreseImagen, new AbsoluteConstraints(190, 240, 230, 30));
 
         txtIngreseSonido.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         txtIngreseSonido.addActionListener(new ActionListener() {
@@ -165,41 +165,41 @@ public class CrearPregYRespDlg  extends JFrame {
                 txtIngreseSonidoActionPerformed(evt);
             }
         });
-        getContentPane().add(txtIngreseSonido, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 280, 230, 30));
-        getContentPane().add(lineRespuestas, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 330, 670, 10));
+        getContentPane().add(txtIngreseSonido, new AbsoluteConstraints(190, 280, 230, 30));
+        getContentPane().add(lineRespuestas, new AbsoluteConstraints(60, 330, 670, 10));
 
         lblIcoTexto.setIcon(new ImageIcon(getClass().getResource("images/IcoTexto.png"))); // NOI18N
         lblIcoTexto.setLabelFor(panel);
-        getContentPane().add(lblIcoTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 380, 30, 30));
+        getContentPane().add(lblIcoTexto, new AbsoluteConstraints(90, 380, 30, 30));
 
         lblIcoSonido.setIcon(new ImageIcon(getClass().getResource("images/IcoSonido.png"))); // NOI18N
-        getContentPane().add(lblIcoSonido, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 380, 30, 30));
+        getContentPane().add(lblIcoSonido, new AbsoluteConstraints(410, 380, 30, 30));
 
         lblPregunta.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         lblPregunta.setText("Pregunta(complete uno o mas campos)");
-        getContentPane().add(lblPregunta, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 160, 340, 30));
+        getContentPane().add(lblPregunta, new AbsoluteConstraints(70, 160, 340, 30));
 
         lblIcoValidar.setIcon(new ImageIcon(getClass().getResource("images/IcoValidar.jpg"))); // NOI18N
-        getContentPane().add(lblIcoValidar, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 380, -1, -1));
+        getContentPane().add(lblIcoValidar, new AbsoluteConstraints(570, 380, -1, -1));
 
         lblIcoImagen.setIcon(new ImageIcon(getClass().getResource("images/IcoImagen.png"))); // NOI18N
-        getContentPane().add(lblIcoImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 380, 30, 30));
+        getContentPane().add(lblIcoImagen, new AbsoluteConstraints(250, 380, 30, 30));
 
         btnProbar.setBackground(new Color(0, 102, 102));
         btnProbar.setFont(new Font("Comic Sans MS", 1, 12)); // NOI18N
         btnProbar.setForeground(new Color(0, 102, 102));
         btnProbar.setText("Probar");
-        getContentPane().add(btnProbar, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 520, 80, 30));
+        getContentPane().add(btnProbar, new AbsoluteConstraints(250, 520, 80, 30));
 
         btnGuardar.setBackground(new Color(0, 102, 102));
         btnGuardar.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         btnGuardar.setText("Guardar");
-        getContentPane().add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 520, 80, 30));
+        getContentPane().add(btnGuardar, new AbsoluteConstraints(360, 520, 80, 30));
 
         btnCancelar.setBackground(new Color(0, 102, 102));
         btnCancelar.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         btnCancelar.setText("Cancelar");
-        getContentPane().add(btnCancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 520, 80, 30));
+        getContentPane().add(btnCancelar, new AbsoluteConstraints(460, 520, 80, 30));
         btnCancelar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
@@ -211,7 +211,7 @@ public class CrearPregYRespDlg  extends JFrame {
         btnArchivoSonido.setBackground(new Color(0, 102, 102));
         btnArchivoSonido.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         btnArchivoSonido.setText("Archivo");
-        getContentPane().add(btnArchivoSonido, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 280, 80, 30));
+        getContentPane().add(btnArchivoSonido, new AbsoluteConstraints(430, 280, 80, 30));
 
         comboGrado.setBackground(new Color(0, 102, 102));
         comboGrado.setEditable(true);
@@ -222,9 +222,8 @@ public class CrearPregYRespDlg  extends JFrame {
                 comboGradoActionPerformed(evt);
             }
         });
-        getContentPane().add(comboGrado, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 60, 90, 30));
+        getContentPane().add(comboGrado, new AbsoluteConstraints(350, 60, 90, 30));
 
-        panel.setFont(new Font("Comic Sans MS", 0, 11)); // NOI18N
 
         tablaRespuestas.setBorder(BorderFactory.createCompoundBorder());
         tablaRespuestas.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
@@ -254,25 +253,25 @@ public class CrearPregYRespDlg  extends JFrame {
         tablaRespuestas.setMinimumSize(new Dimension(10, 10));
         panel.setViewportView(tablaRespuestas);
 
-        getContentPane().add(panel, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 380, 650, 120));
+        getContentPane().add(panel, new AbsoluteConstraints(80, 380, 650, 120));
 
         btnAgregar.setBackground(new Color(0, 102, 102));
         btnAgregar.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         btnAgregar.setText("Agregar");
-        getContentPane().add(btnAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 340, 80, 30));
+        getContentPane().add(btnAgregar, new AbsoluteConstraints(640, 340, 80, 30));
 
         btnArchivoImagen.setBackground(new Color(0, 102, 102));
         btnArchivoImagen.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         btnArchivoImagen.setText("Archivo");
-        getContentPane().add(btnArchivoImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 240, 80, 30));
+        getContentPane().add(btnArchivoImagen, new AbsoluteConstraints(430, 240, 80, 30));
 
         lblTema.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         lblTema.setText("Tema:");
-        getContentPane().add(lblTema, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 120, 60, 30));
+        getContentPane().add(lblTema, new AbsoluteConstraints(120, 120, 60, 30));
 
         lblMaxIntentos.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         lblMaxIntentos.setText("Max.Intentos:");
-        getContentPane().add(lblMaxIntentos, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 60, 90, 30));
+        getContentPane().add(lblMaxIntentos, new AbsoluteConstraints(560, 60, 90, 30));
 
         txtIngreseTexto.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         txtIngreseTexto.addActionListener(new ActionListener() {
@@ -280,7 +279,7 @@ public class CrearPregYRespDlg  extends JFrame {
                 txtIngreseTextoActionPerformed(evt);
             }
         });
-        getContentPane().add(txtIngreseTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 200, 520, 30));
+        getContentPane().add(txtIngreseTexto, new AbsoluteConstraints(190, 200, 520, 30));
 
         txtTema.setFont(new Font("Comic Sans MS", 0, 14)); // NOI18N
         txtTema.addActionListener(new ActionListener() {
@@ -288,7 +287,7 @@ public class CrearPregYRespDlg  extends JFrame {
                 txtTemaActionPerformed(evt);
             }
         });
-        getContentPane().add(txtTema, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 520, 30));
+        getContentPane().add(txtTema, new AbsoluteConstraints(170, 120, 520, 30));
 
         comboNivel.setBackground(new Color(0, 102, 102));
         comboNivel.setEditable(true);
@@ -299,7 +298,7 @@ public class CrearPregYRespDlg  extends JFrame {
                 comboNivelActionPerformed(evt);
             }
         });
-        getContentPane().add(comboNivel, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 60, 80, 30));
+        getContentPane().add(comboNivel, new AbsoluteConstraints(260, 60, 80, 30));
 
         comboDificultad.setBackground(new Color(0, 102, 102));
         comboDificultad.setEditable(true);
@@ -310,17 +309,17 @@ public class CrearPregYRespDlg  extends JFrame {
                 comboDificultadActionPerformed(evt);
             }
         });
-        getContentPane().add(comboDificultad, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 60, 90, 30));
+        getContentPane().add(comboDificultad, new AbsoluteConstraints(450, 60, 90, 30));
 
         comboMateria.setBackground(new Color(0, 102, 102));
         comboMateria.setEditable(true);
         comboMateria.setForeground(new Color(0, 102, 102));
         comboMateria.setModel(new DefaultComboBoxModel<>(new String[] { "Materia" }));
-        getContentPane().add(comboMateria, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 60, 160, 30));
+        getContentPane().add(comboMateria, new AbsoluteConstraints(90, 60, 160, 30));
 
         lblTitulo.setFont(new Font("Comic Sans MS", 0, 18)); // NOI18N
         lblTitulo.setText("Crear Pregunta y Respuestas");
-        getContentPane().add(lblTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, 260, 50));
+        getContentPane().add(lblTitulo, new AbsoluteConstraints(270, 10, 260, 50));
 
         lblFondo.setFont(new Font("Comic Sans MS", 0, 12)); // NOI18N
         lblFondo.setIcon(new ImageIcon(getClass().getResource("images/Fondo2.jpg"))); // NOI18N
@@ -329,7 +328,7 @@ public class CrearPregYRespDlg  extends JFrame {
         lblFondo.setMaximumSize(new Dimension(800, 600));
         lblFondo.setMinimumSize(new Dimension(800, 600));
         lblFondo.setName(""); // NOI18N
-        getContentPane().add(lblFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 590));
+        getContentPane().add(lblFondo, new AbsoluteConstraints(0, 0, 800, 590));
 
         pack();
     }                   
