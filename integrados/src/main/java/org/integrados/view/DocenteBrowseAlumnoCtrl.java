276230b81c1b52f25d1a5ee730de8d6b12d1db3c
Yani <yanina.gallardo30@gmail.com>
2019-03-04 16:42:37 -0300

    ventana que muestra la lista de alumnos que posee el docente. (falta funciones de los botones)

---
+++ integrados/src/main/java/org/integrados/view/DocenteBrowseAlumnoCtrl.java
@@ -0,0 +1,63 @@
+
+package org.integrados.view;
+
+import java.util.List;
+import org.integrados.Aplicacion;
+import org.integrados.controller.ABM.PersonaABM;
+import org.integrados.data.actividad.RegistroActividad;
+import org.integrados.data.usuarios.Alumno;
+import org.integrados.data.usuarios.Docente;
+
+public class DocenteBrowseAlumnoCtrl {
+
+    private Aplicacion app;
+    private DocenteBrowseAlumnoDlg alumnoBrw;
+    public DocenteInicioDlg docenteInicioDlg;
+    private PersonaABM alumnoABM = new PersonaABM();
+    private List<Alumno> listaAlumnos;
+    private int contadorRealizadas = 0;
+    private int contadorNoRealizadas = 0;
+
+    public DocenteBrowseAlumnoCtrl(DocenteInicioDlg docenteInicioDlg) {
+        this.docenteInicioDlg = docenteInicioDlg;
+    }
+
+    public void mostrarBrw() {
+        Docente docente = this.docenteInicioDlg.getDocenteInicioCtrl().getDocente();
+        listaAlumnos = alumnoABM.listaAlumnos(docente.getId());
+        this.cantRealizadasYNo(listaAlumnos);
+
+        alumnoBrw = new DocenteBrowseAlumnoDlg(this, listaAlumnos, this.contadorRealizadas, this.contadorNoRealizadas);
+        this.alumnoBrw.mostrar();
+    }
+
+    public void cantRealizadasYNo(List<Alumno> alumnos) {
+
+        for (Alumno alumno : alumnos) {
+            for (RegistroActividad reg : alumno.getRegistroActividades()) {
+                try {
+                    if (reg.getFinalizoCorrectamente()) {
+                        this.contadorRealizadas++;
+                    } else { 
+                        this.contadorNoRealizadas++;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error en metodo cantRealizadasYNo");
+                }
+            }
+        }
+    }
+
+    public void ocultar() {
+        alumnoBrw.setVisible(false);
+    }
+
+    public void cerrarAplicacion() {
+        Dialogo.ResultadoDialogo resultado = Dialogo.confirmacion("¡Atención!", "¿Realmente desea salir?");
+        if (resultado == Dialogo.ResultadoDialogo.Yes) {
+            this.alumnoBrw.ocultar();
+            //this.app.cerrar();   ----> ERROR  Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException
+        }
+    }
+
+}
---
+++ integrados/src/main/java/org/integrados/view/DocenteBrowseAlumnoDlg.java
@@ -0,0 +1,314 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.integrados.view;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.AbstractTableModel;
+import org.integrados.data.actividad.Actividad;
+import org.integrados.data.usuarios.Alumno;
+import org.integrados.data.util.Util;
+
+/**
+ *
+ * @author Yani
+ */
+public class DocenteBrowseAlumnoDlg extends JFrame {
+    private DocenteBrowseAlumnoCtrl controlador;
+    private List<Alumno> listaAlumnos = new ArrayList<>();
+    private int contadorRealizadas;
+    private int contadorNoRealizadas;
+    private JLabel lblFondo;
+    private JPanel pnlBotonesEdicion = null;
+    public JButton btnDatos = null;
+    public JButton btnRealizadas = null;
+    public JButton btnSinRealizar = null;
+    public JButton btnVolver = null;
+    private JTable tablaAlumnos = null;
+    
+    public DocenteBrowseAlumnoDlg(DocenteBrowseAlumnoCtrl controlador, List<Alumno> listaAlumnos, int realizadas, int noRealizadas){
+        this.controlador = controlador;
+        this.listaAlumnos = listaAlumnos;
+        this.contadorRealizadas=realizadas;
+        this.contadorNoRealizadas=noRealizadas;
+        initComponent();
+    }
+    
+    private void initComponent(){
+        this.setTitle("Lista de Alumnos");
+        this.pack();
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                controlador.cerrarAplicacion();
+            }
+        });            
+        
+         JPanel pnlCentral = new JPanel();
+        BorderLayout pnlCentralLayout = new BorderLayout();
+        pnlCentral.setOpaque(false);
+        pnlCentral.setLayout(pnlCentralLayout);
+        
+        lblFondo = new JLabel();        
+        // Propiedades del fondo de pantalla
+        ImageIcon icon = createImageIcon("images/Fondo.jpg","Fondo");
+        lblFondo.setIcon(icon);
+//        lblFondo.setBounds(0, 0, 800, 600);
+        lblFondo.setLayout(new BorderLayout());        
+
+        // Propiedades de la tabla de alumnos
+        tablaAlumnos = new JTable();
+        tablaAlumnos.setOpaque(true);           
+        
+        tablaAlumnos.setBackground(new java.awt.Color(250, 255, 113));
+        tablaAlumnos.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(237, 90, 39)));
+        tablaAlumnos.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); 
+        
+        tablaAlumnos.setAlignmentX(1.0F);
+        tablaAlumnos.setAlignmentY(1.0F);
+        tablaAlumnos.setGridColor(new java.awt.Color(237, 90, 39));
+        tablaAlumnos.setIntercellSpacing(new java.awt.Dimension(0, 0));
+        tablaAlumnos.setSelectionBackground(new java.awt.Color(85, 196, 190));           
+        
+        JScrollPane jScrollPane = new JScrollPane(tablaAlumnos);
+        jScrollPane.setOpaque(false);
+        jScrollPane.getViewport().setOpaque(false);
+        
+        tablaAlumnos.setModel(new AlumnosTableModel(listaAlumnos, this.contadorRealizadas, this.contadorNoRealizadas));
+
+        tablaAlumnos.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    editarActividadActual();
+                }
+            }
+        });
+        pnlCentral.add(jScrollPane, BorderLayout.CENTER);
+        lblFondo.add(pnlCentral, BorderLayout.CENTER);
+
+        pnlBotonesEdicion = new JPanel();
+        pnlBotonesEdicion.setLayout(null);
+        pnlBotonesEdicion.setOpaque(false);
+        pnlBotonesEdicion.setPreferredSize(new java.awt.Dimension(300, 35));
+        pnlBotonesEdicion.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+
+        // Propiedades del botón Volver
+        btnVolver = Util.crearBoton("Volver", 12);
+        btnVolver.setBounds(650, 7, 90, 22);
+        pnlBotonesEdicion.add(btnVolver);
+        btnVolver.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                volver();
+            }
+        });
+        
+        // Propiedades del botón datos personales
+        btnDatos = Util.crearBoton("Datos Personales", 12);
+        btnDatos.setBounds(40, 7, 120, 22);
+        pnlBotonesEdicion.add(btnDatos);
+        btnDatos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                Dialogo.mensaje(" En construcción ", " ¡Estamos trabajando para usted! ");
+                //ventana datos personales del alumno..  solo mostrar
+            }
+        });
+        
+         // Propiedades del botón Actividades realizadas
+        btnRealizadas = Util.crearBoton("Ver Actividades Realizadas", 12);
+        btnRealizadas.setBounds(200, 7, 180, 22);
+        pnlBotonesEdicion.add(btnRealizadas);
+        btnRealizadas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                Dialogo.mensaje(" En construcción ", " ¡Estamos trabajando para usted! ");
+                //ventana actividades realizadas (pasar lista de registroActividad del alumno);
+            }
+        });
+        
+        // Propiedades del botón Actividades sin realizar
+        btnSinRealizar = Util.crearBoton("Ver Actividades Sin Realizar", 12);
+        btnSinRealizar.setBounds(390, 7, 200, 22);
+        pnlBotonesEdicion.add(btnSinRealizar);
+        btnSinRealizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                Dialogo.mensaje(" En construcción ", " ¡Estamos trabajando para usted! ");
+                //ventana actividades sin realizar (pasar lista de registroActividad del alumno);
+            }
+        });
+        
+         getContentPane().add(pnlBotonesEdicion, BorderLayout.SOUTH);
+        
+        this.add(lblFondo);
+        
+        this.mostrar();
+        
+    }
+    
+    
+    
+    protected ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.out.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+    
+      public void volver() {
+        this.controlador.ocultar();
+        this.controlador.docenteInicioDlg.initComponents();
+    }
+    
+      private void editarActividadActual() {
+        System.out.println("editarActividad");
+    	Alumno alumno = getAlumnoSeleccionado();
+
+    	if (alumno != null) {
+//            this.controlador.editar(actividad);
+            Dialogo.error("En construcción","¡Estamos trabajando para usted!");
+    	}
+    }
+      
+      private Alumno getAlumnoSeleccionado() {
+    	int indexSeleccionado = this.tablaAlumnos.getSelectedRow();
+    	return ((AlumnosTableModel)this.tablaAlumnos.getModel()).getAlumno(indexSeleccionado);
+    }
+      
+    
+    public void mostrar() {
+        this.setVisible(true);
+    }
+    
+    
+    public void ocultar() {
+        this.setVisible(false);
+    } 
+    
+    
+    
+    /**
+     * Modelo de la JTable de Actividades.
+     */
+    public class AlumnosTableModel extends AbstractTableModel {
+
+        protected List<Alumno> alumnos;
+        protected int contadorRealizadas;
+        protected int contadorNoRealizadas;
+
+        public AlumnosTableModel(List<Alumno> alumnos, int realizadas, int noRealizadas) {
+            this.alumnos = alumnos;
+            this.contadorRealizadas=realizadas;
+            this.contadorNoRealizadas=noRealizadas;
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            if (column == 0) {
+                return "Nombre";
+            } else if (column == 1) {
+                return "Apellido";
+            } else if (column == 2) {
+                return "Cant. Actividades Realizadas";
+            } else if (column == 3) {
+                return "Cant. Actividades Sin Realizar";
+            }  else 
+                return "Observaciones";    
+        }
+
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+
+        @Override
+        public Class getColumnClass(int column) {
+            if (column == 0) {
+                return String.class;
+            } else if (column == 1) {
+                return String.class;
+            } else if (column == 2) {
+                return String.class;
+            }  else if (column == 3) {
+                return String.class;
+            }  else {
+                return String.class;
+            }
+        }
+
+        @Override
+        public Object getValueAt(int row, int column) {
+            Alumno alumno = alumnos.get(row);
+            if (column == 0) {
+                return alumno.getNombre();
+            } else if (column == 1) {
+                return alumno.getApellido();
+            } else if (column == 2) {
+                return this.contadorRealizadas;
+            } else if (column == 3) {
+                return this.contadorNoRealizadas;
+            } else {
+                return alumno.getObservaciones();
+            }
+        }
+
+        @Override
+        public void setValueAt(Object value, int row, int column) {
+        }
+
+        @Override
+        public int getRowCount() {
+            return alumnos.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 5;
+        }
+
+        public Alumno getAlumno(int index) {
+            if ((index < 0) || (index > alumnos.size())) {
+                return null;
+            } else {
+                return alumnos.get(index);
+            }
+        }
+    }    
+    
+}
+    
+    
+    
+    
+    
+    
+
+
+
