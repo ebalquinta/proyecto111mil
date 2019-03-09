5e86f52f55740fa5fc6ef628517e1d190b31b755
jacco <jaccogriffoen@gmail.com>
2019-03-09 13:35:52 -0300

    nada

--- integrados/src/main/java/org/integrados/test/MemoramaTest.java
+++ integrados/src/main/java/org/integrados/test/MemoramaTest.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.integrados.test;
+package org.integrados.test; 
+
 
 import java.util.ArrayList;
 import java.util.List;
